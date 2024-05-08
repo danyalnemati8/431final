caab37ffc0d4c97727393eed1c964a0175ab6d2a
Danyal Nemati <Dnemati@csu.fullerton.edu>
2024-05-07 18:10:38 -0700

    added delete button and chnaged colors for lists

--- src/List.js
+++ src/List.js
@@ -2,8 +2,8 @@
 
 const List = () => {
     return (
-        <div className="h-[90%] w-1/5 flex flex-col justify-start items-center m-3 bg-black rounded-2xl">
-            <h2 className="h-[10%] w-full text-center text-white m-1">
+        <div className="h-[90%] w-1/5 flex flex-col justify-start items-center m-3 bg-white rounded-2xl">
+            <h2 className="h-[10%] w-full text-center text-black m-1">
                 List Name
             </h2>
             <div className="h-[90%] w-full text-center flex flex-col justify-start items-center">
@@ -14,10 +14,16 @@ const List = () => {
 }
 
 const ListItem = () => {
+    const handleDelete = () => {
+        console.log("Delete clicked")
+    }
     return (
-        <div className="w-5/6 h-1/6 flex justify-between items-center bg-red-200 p-2 rounded-xl">
-            <h3 className="text-white">Item Name</h3>
-            <input type="checkbox"/>
+        <div className="w-5/6 h-1/6 flex flex-col justify-between bg-blue-400 p-2 rounded-xl">
+            <div className="flex justify-between items-center">
+                <h3 className="text-black">Item Name</h3>
+                <input type="checkbox"/>
+            </div>
+            <button className="mt-2 text-black" onClick={handleDelete}>Delete</button>
         </div>
     )
 }
