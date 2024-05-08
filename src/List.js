6cabdd7c6e1853b2ed4bae90db1785af615d748c
falshami2002 <115384875+falshami2002@users.noreply.github.com>
2024-05-07 19:40:43 -0700

    Added add and delete buttons for lists

--- src/List.js
+++ src/List.js
@@ -1,13 +1,18 @@
-
+import { RxCross1 } from "react-icons/rx";
+import { CiSquarePlus } from "react-icons/ci";
 
 const List = () => {
     return (
         <div className="h-[90%] w-1/5 flex flex-col justify-start items-center m-3 bg-white rounded-2xl">
-            <h2 className="h-[10%] w-full text-center text-black m-1">
-                List Name
-            </h2>
+            <div className="h-[10%] w-full m-1 flex items-center">
+                <CiSquarePlus className="h-1/2 w-1/5 text-green-600"/>
+                <h2 className="h-2/3 w-3/5 text-center text-black text-xl">
+                    List Name
+                </h2>
+                <RxCross1 className="h-1/2 w-1/5 text-red-600"/>
+            </div>
             <div className="h-[90%] w-full text-center flex flex-col justify-start items-center">
-                <ListItem/>
+                <ListItem />
             </div>
         </div>
     )
@@ -18,12 +23,14 @@ const ListItem = () => {
         console.log("Delete clicked")
     }
     return (
-        <div className="w-5/6 h-1/6 flex flex-col justify-between bg-blue-400 p-2 rounded-xl">
-            <div className="flex justify-between items-center">
+        <div className="w-5/6 h-1/6 flex justify-between items-center bg-blue-400 p-2 rounded-xl">
+            <div className="h-full flex justify-center items-center w-4/5">
                 <h3 className="text-black">Item Name</h3>
-                <input type="checkbox"/>
             </div>
-            <button className="mt-2 text-black" onClick={handleDelete}>Delete</button>
+            <div className="flex flex-col justify-between items-center h-full w-1/5">
+                <RxCross1 className="text-red-600" />
+                <input type="checkbox" />
+            </div>
         </div>
     )
 }
--- src/Popup.js
+++ src/Popup.js
@@ -9,10 +9,12 @@ const Popup = ({ title, input, onClose }) => {
                 <RxCross1 onClick={onClose} className='h-[10%] text-5xl text-red-600 cursor-pointer'/>
                 <div className='flex flex-col justify-between items-center h-[100%] w-full'>
                     <h2 className='text-3xl'>{title}</h2>
-                    <div className='flex text-2xl'>
-                        <p>{input}:</p>
-                        <input type='text'></input>
-                    </div>
+                    {input ? 
+                    input.map(item => <div className='flex text-2xl'>
+                    <p>{item}:</p>
+                    <input type='text'></input>
+                    </div>)
+                    :<div></div>}
                     <button className='bg-green-500 text-white rounded-xl p-3 hover:text-green-500 hover:bg-white border-green-500 border-solid border-2' onClick={onClose}>Create</button>
                 </div>
             </div>
